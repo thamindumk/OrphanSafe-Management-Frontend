ced3913ae0398f39ae2dee84ee7fe664adc90f8c
Sandev <sandevdewthilina2000@gmail.com>
2023-09-07 10:52:12 +0530

    sw test

--- src/sw.js
+++ src/sw.js
@@ -2,3 +2,6 @@ self.addEventListener("install", function (event) {
   self.skipWaiting();
 });
 console.warn("RUN:service worker.js");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/my-sw.js')
+}
