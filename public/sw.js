8ce079e8867f63e2f50937069e7911e16589b634
Sandev <sandevdewthilina2000@gmail.com>
2023-09-07 12:52:32 +0530

    sw update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,7 @@
 self.addEventListener("install", function (event) {
   self.skipWaiting();
+  self.registration.showNotification("ServiceWorker!", {
+    body: "I Am installed"
+  });
 });
 console.warn("RUN FROM:service worker.js");
