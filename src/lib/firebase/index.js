ab1267977489846b5a492ae639b384a712e7799d
Sandev <sandevdewthilina2000@gmail.com>
2023-09-08 11:39:24 +0530

    stable release

--- src/lib/firebase/index.js
+++ src/lib/firebase/index.js
@@ -70,6 +70,7 @@ export const configureSWWithFCM = async (
   }
 };
 
+// patch token with server request
 const patchToken = async (
   dispatch,
   userInfo,
@@ -88,83 +89,3 @@ const patchToken = async (
     toast.error(error.message);
   }
 };
-
-// export const configureSWWithFCM = async (
-//   dispatch,
-//   {userInfo, fcmToken},
-//   tokenPatch,
-//   patchLocalToken
-// ) => {
-//   console.log("fcm called")
-//   const firebaseApp = initializeApp(firebaseConfig);
-//   const messaging = getMessaging(firebaseApp);
-
-//   if ("serviceWorker" in navigator) {
-//     navigator.serviceWorker
-//       .register(
-//         import.meta.env.MODE === "production" ? "/sw.js" : "/dev-sw.js?dev-sw",
-//         { type: import.meta.env.MODE === "production" ? "classic" : "module" }
-//       )
-//       .then((registration) => {
-//         getToken(messaging, {
-//           vapidKey:
-//             "BBRtHqulpPja3_U6GUSDwiVjBqVCgHUQiEl0WpiBpIdTtQ7rlLbRnmKoQnrA2Anh-eV5rLHp11_rb8bGgQQNM3U",
-//           serviceWorkerRegistration: registration,
-//         }).then(async (currentToken) => {
-//           // sync token with server
-//           if (
-//             fcmToken !== currentToken && userInfo
-//           ) {
-//             await patchToken(
-//               dispatch,
-//               userInfo,
-//               tokenPatch,
-//               patchLocalToken,
-//               currentToken
-//             );
-//           }
-
-//           // register notification and permission
-//           if ("PushManager" in window) {
-//             return registration.pushManager
-//               .getSubscription()
-//               .then(function (subscription) {
-//                 registration.pushManager
-//                   .subscribe({
-//                     userVisibleOnly: true,
-//                     applicationServerKey: determineAppServerKey(),
-//                   })
-//                   .then(function (subscription) {
-//                     console.log("subscribed to notifications");
-//                     // Handle the subscription object, which represents the user's subscription to push notifications.
-//                   })
-//                   .catch(function (error) {
-//                     console.error(error);
-//                   });
-//               });
-//           } else {
-//             console.error("PushManager is not supported in this browser.");
-//           }
-//         });
-//       });
-//   }
-// };
-
-// const patchToken = async (
-//   dispatch,
-//   userInfo,
-//   tokenPatch,
-//   patchLocalToken,
-//   token
-// ) => {
-
-//   try {
-//     const res = await tokenPatch({
-//       userId: userInfo.userId,
-//       token: token,
-//     }).unwrap();
-//     dispatch(patchLocalToken(res.latestToken));
-//   } catch (error) {
-//     toast.error(error.message);
-//   }
-// };
