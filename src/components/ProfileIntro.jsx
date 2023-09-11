52de3a8f693fe926f037e54d0560287f2a55e75d
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-09-12 01:15:38 +0530

    profile make responsive

--- src/components/ProfileIntro.jsx
+++ src/components/ProfileIntro.jsx
@@ -7,14 +7,10 @@ const ProfileIntro = () => {
     <div className="card">
       <div className="background">
         <Row className="profile">
-          <Col sm={12} md={12} lg={6} xl={4}>
-            <Image
-              className="profileImage"
-              src="https://i.guim.co.uk/img/media/60ef89fa5cc775cea493523a8c4b354c8f58faf2/153_0_3288_1974/master/3288.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=a68540f4bf558e4f7e4887d14a934547"
-              alt="profile"
-            ></Image>
+          <Col xs={12} sm={12} md={4} lg={6} xl={4}>
+            <div className="profileImage"></div>
           </Col>
-          <Col sm={12} md={12} lg={6} xl={8}>
+          <Col xs={12} sm={12} md={8} lg={6} xl={8}>
             <h1 className="name-header">Thamindu Manodya</h1>
             <p className="name-sub">12 years old</p>
             <p className="name-sub">Male</p>
