09b6eead441e8168644856c6e801fdd5c2eef934
Ravini <ravinikuruppu@gmail.com>
2023-10-09 12:18:50 +0530

    staff, social worker over view

--- src/components/external-party/ParentViewChildProfile.jsx
+++ src/components/external-party/ParentViewChildProfile.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import { Form, Table, Row, Col } from "react-bootstrap";
 import { MyCard, MyCardBody, MyCardHeader } from "../../components/MyCard";
-
+import {
+  useViewChildProfilesQuery,
+} from "../../slices/profileApiSlice";
+import "../../assets/css/profileIntro.css"
 const ParentViewChildProfile = () => {
-  return (
+ 
+  const queryParams = new URLSearchParams(location.search);
+  const paramValue = queryParams.get("childId");
+  const {data, isLoading,isError,isSuccess}=
+  useViewChildProfilesQuery(paramValue);
+
+  return isSuccess && (
     <div>
+
+<div className="card">
+      <div className="background">
+        <Row className="profile">
+          <Col xs={12} sm={12} md={4} lg={6} xl={4}>
+            <div className="profileImage"></div>
+          </Col>
+          <Col xs={12} sm={12} md={8} lg={6} xl={8}>
+            <h1 className="name-header">{data.childProfile.FullName}</h1>
+            <p className="name-sub">Orphanage Name:</p>
+            <p className="name-sub">{data.childProfile.OrphanageName}</p>
+            <p className="name-sub">{data.childProfile.Gender}</p>
+          </Col>
+        </Row>
+      </div>
+    </div>
+
+
       <Row>
         <Col sm={0} md={1} lg={1}></Col>
         <Col sm={12} md={10} lg={10}>
@@ -14,37 +41,145 @@ const ParentViewChildProfile = () => {
               <Form>
                 <Table responsive bordered style={{ width: "100%" }}>
                   <tbody>
+                 
                     <tr>
                       <td className="td">Name with initials</td>
-                      <td className="td">K.D Kariyawasam</td>
+                      <td className="td">{data.childProfile.FullName}</td>
                     </tr>
                     <tr>
                       <td className="td">Birth Day</td>
-                      <td className="td">1980/10/13</td>
+                      <td className="td">{data.childProfile.DOB}</td>
                     </tr>
                     <tr>
                       <td className="td">Gender</td>
-                      <td className="td">Male</td>
+                      <td className="td">{data.childProfile.Gender}</td>
                     </tr>
                     <tr>
-                      <td className="td">Type of Employee</td>
-                      <td className="td">Orphanage manager</td>
+                      <td className="td">Birth City</td>
+                      <td className="td">{data.childProfile.City}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td className="td">Nationality</td>
+                      <td className="td">{data.childProfile.Nationality}</td>
                     </tr>
                     <tr>
-                      <td className="td">Address</td>
+                      <td className="td">Language</td>
+                      <td className="td">{data.childProfile.Language}</td>
+                    </tr>
+                    
+                    
+                  </tbody>
+                </Table>
+              </Form>
+            </MyCardBody>
+          </MyCard>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={0} md={1} lg={1}></Col>
+        <Col sm={12} md={10} lg={10}>
+          <MyCard>
+            <MyCardHeader>Additional Information</MyCardHeader>
+            <MyCardBody>
+              <Form>
+                <Table responsive bordered style={{ width: "100%" }}>
+                  <tbody>
+                 
+                    <tr>
+                      <td className="td">Medical history</td>
+                      <td className="td">{data.childProfile.MedicalDesc}</td>
+                    </tr>
+                    <tr>
+                      <td className="td">Remarks</td>
+                      <td className="td">{data.childProfile.Remark}</td>
+                    </tr>
+                    <tr>
+                      <td className="td">Reason for Placement</td>
+                      <td className="td">{data.childProfile.ReasonForPlacement}</td>
+                    </tr>
+                    {/* <tr>
+                      <td className="td">Medical Documents</td>
                       <td className="td">
-                        7A/3 , Kahatawatta, Panadura, Kalutara
+                        <a href="#" class="blue-button">
+                          View
+                        </a>
+                      </td>
+                    </tr> */}
+                  </tbody>
+                </Table>
+              </Form>
+            </MyCardBody>
+          </MyCard>
+        </Col>
+      </Row>
+      
+      <Row>
+        <Col sm={0} md={1} lg={1}></Col>
+        <Col sm={12} md={10} lg={10}>
+          <MyCard>
+            <MyCardHeader>Family Background</MyCardHeader>
+            <MyCardBody>
+              <Form>
+                <Table responsive bordered style={{ width: "100%" }}>
+                  <tbody>
+                    <tr>
+                      <td className="td">Father's Name</td>
+                      <td className="td">{data.childProfile.BirthFather}</td>
+                    </tr>
+                    <tr>
+                      <td className="td">Mother's Name</td>
+                      <td className="td">{data.childProfile.BirthMother}</td>
+                    </tr>
+                    
+                  </tbody>
+                </Table>
+              </Form>
+            </MyCardBody>
+          </MyCard>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={0} md={1} lg={1}></Col>
+        <Col sm={12} md={10} lg={10}>
+          <MyCard>
+            <MyCardHeader>Registration Information</MyCardHeader>
+            <MyCardBody>
+              <Form>
+                <Table responsive bordered>
+                  <tbody>
+                    <tr>
+                      <td className="td">Date Of Admission</td>
+                      <td className="td">{data.childProfile.DateOfAdmission}</td>
+                    </tr>
+                    <tr>
+                      <td className="td">Orphanage Name</td>
+                      <td className="td">{data.childProfile.OrphanageName}</td>
+                    </tr>
+                    {/* <tr>
+                      <td className="td">Guardian Contact Number</td>
+                      <td className="td">0765648734</td>
+                    </tr>
+                    <tr>
+                      <td className="td">Guardian relationship to the child</td>
+                      <td className="td">Staff member</td>
+                    </tr>
+                    <tr>
+                      <td className="td">Legal Documents</td>
+                      <td className="td">
+                        <a href="#" class="blue-button">
+                          View
+                        </a>
                       </td>
                     </tr>
-
                     <tr>
-                      <td className="td">NIC number</td>
-                      <td className="td">8025102799</td>
-                    </tr>
-                    <tr>
-                      <td className="td">Email</td>
-                      <td className="td">kariyawasamkd@gmail.com</td>
-                    </tr>
+                      <td className="td">Cases</td>
+                      <td className="td">
+                        <a href="#" class="blue-button">
+                          View
+                        </a>
+                      </td>
+                    </tr> */}
                   </tbody>
                 </Table>
               </Form>
